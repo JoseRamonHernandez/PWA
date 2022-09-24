dc9969f4c1e84f7bafbd8c3f44070c54edaab37f
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2022-09-23 19:25:26 -0600

    manejo de errores

--- prom-2.js
+++ prom-2.js
@@ -2,24 +2,39 @@ function sumarUno( numero ){
 
     var promesa = new Promise((resolve, reject)=>{
 
+        if(numero >= 7){
+            reject('numero muy alto');
+        }
+
         setTimeout(()=>{
 
             resolve(numero + 1);
-
-        }, 900)
-
+        }, 800)
     })
 
 
     return promesa
 }
 
+
 sumarUno(5).then( nuevoNumero =>{
-        console.log(nuevoNumero);
-        return sumarUno(nuevoNumero)
-    }).then(nuevoNumero=>{
-        console.log(nuevoNumero);
-        return sumarUno(nuevoNumero)
-    }).then(nuevoNumero=>{
-        console.log(nuevoNumero);
+    console.log(nuevoNumero);
+    return sumarUno(nuevoNumero)
+}).then(nuevoNumero=>{
+    console.log(nuevoNumero);
+    return sumarUno(nuevoNumero)
+}).then(nuevoNumero=>{
+sumarUno(5)
+.then(sumarUno)
+.then(sumarUno)
+.then(sumarUno)
+.then(sumarUno)
+.then(nuevoNumero=>{
+    console.log(nuevoNumero);
+})
+.catch(err=>{
+    console.log('Error en promesa');
+    console.log(err);
+})
+
 })
