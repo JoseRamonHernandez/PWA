b862f46525e21034c843cd3a9ee74081578c9fca
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2022-10-03 09:37:15 -0600

    manejo de errores

--- 02-service-worker/sw.js
+++ 02-service-worker/sw.js
@@ -4,19 +4,15 @@ self.addEventListener('fetch', event => {
 
     if(event.request.url.includes('style.css'))
     {
-        let respuesta = new Response(`
-        body {
-            background-color: red !important;
-            color: pink;
-        }
-        `,{
-            headers:{
-                'Content-type': 'text/css'
-            }
-        
+        const resp = fetch( event.request )
+        .then( resp => {
+            
+            return resp.ok ? resp : fetch('img/main.jpg');
+            
         });
 
-            event.respondWith(respuesta);
+    
+    event.respondWith(resp);
     }
 
 });
