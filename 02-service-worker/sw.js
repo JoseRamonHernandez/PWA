c0759648be13e95d56221c67ea21e4509722cd52
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2022-10-03 08:45:37 -0600

    fetch event

--- 02-service-worker/sw.js
+++ 02-service-worker/sw.js
@@ -1,1 +1,11 @@
-console.log('SW:_Hola mundo 2');
+
+
+self.addEventListener('fetch', event => {
+
+    if(event.request.url.includes('style.css')){
+        event.respondWith(null);
+    }else{
+        event.respondWith(fetch(event.request));
+    }
+
+});
