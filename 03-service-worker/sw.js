1c36836c748d6068c77284dd5daf298a33404e3b
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2022-10-03 09:46:40 -0600

    activate

--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -3,8 +3,16 @@
 
 self.addEventListener('install', event => {
     //Descargar assets
-    //Creamos un cahe
+    //Creamos un cache
 
     console.log('SW: Instalando SW!!');
 
+});
+
+//Cuando el SW toma  el control de la aplicacion 
+
+self.addEventListener('activate', event => {
+    //Borrar cache viejo
+
+    console.log('SW2: Activo y listo para controlar la app');
 });
